21a3f34224ed5e27c021c58d2a6cef1fda5cf3c7
hrudaniyal <niyaljohn@gmail.com>
2022-04-24 02:58:25 +0530

    implementing unplash api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 function App() {
+  const key = 'LEoP585PKwvAc8YsADGIO-5ma_so9GxKEpxMu-80Olc'
+  const param = 'animal'
+  const [result,setResult] = useState([])
+  useEffect(()=>{
+    apiCall()
+  },[])
+  const apiCall = ()=>{
+    axios.get(`https://api.unsplash.com/search/photos?page=20&X-Ratelimit-Limit=30&query=flower&client_id=${key}`).then(res=>{
+      setResult(res.data.results)
+    })
+  }
+  console.log(result)
   return (
-    <div className='heading'>
+<div>
+<div className='heading'>
       <h1>FLOWER PLANTS</h1>
       <p>the new life with flowers</p>
     </div>
+    <div >
+      {
+        result.map((data)=>{
+            return <div key={data.id} className='img_des' style={{background:data.color,padding:"40px"}}>
+                <div >
+          
+                <img className='show_img' src={data.urls.small} alt={data.alt_description} />  
+
+                </div>
+                <div className='show_des'>
+                <h4>{data.alt_description}</h4>
+                <p>{data.alt_description}</p>
+                <button>START</button>
+                </div>
+            </div>
+        })
+      }
+    </div>
+</div>
   );
 }
 
